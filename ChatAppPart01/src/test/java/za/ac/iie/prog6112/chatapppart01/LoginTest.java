dd3912499b6c47694e2b012ec60d474f95b2e44d
Minqweno <minqwenongati@gmail.com>
2026-09-15 10:49:23 +0200

    updated login test added a username test comment

--- ChatAppPart01/src/test/java/za/ac/iie/prog6112/chatapppart01/LoginTest.java
+++ ChatAppPart01/src/test/java/za/ac/iie/prog6112/chatapppart01/LoginTest.java
@@ -10,4 +10,17 @@ package za.ac.iie.prog6112.chatapppart01;
  */
 public class LoginTest {
     
+    Login login = new Login();
+    
+    //---USERNAME TEST---
+    /*Check if all the conditions were met when the user was creating 
+    their username, if the username met all the conditions then it should
+    be accepted.
+    */
+    @Test
+    public void testValidUserName() {
+        assertTrue(login.checkUserName ("mimi_1"));
+        
+    }
+    
 }
